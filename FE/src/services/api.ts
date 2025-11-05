dd1a5ce74679791a1c353e55d805e5d10b54908f
Rahul IB <rahulib7172@gmail.com>
2025-11-05 17:36:27 +0530

    old one

--- FE/src/services/api.ts
+++ FE/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:9000/api";
+
 // Create axios instance with default config
 const api = axios.create({
   baseURL: API_BASE_URL,
