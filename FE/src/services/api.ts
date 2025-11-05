d44396406511f3f3a390edc47ed02f29194ef096
Rahul IB <rahulib7172@gmail.com>
2025-11-05 17:26:26 +0530

    only prod link no localhost fallback

--- FE/src/services/api.ts
+++ FE/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:9000/api";
-
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 // Create axios instance with default config
 const api = axios.create({
   baseURL: API_BASE_URL,
